e926235b05e077d7d105b7a3443f8d688fff6ee5
Jake <jakeperi168@gmail.com>
2024-01-26 10:16:49 -0500

    1/26

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -5,8 +5,8 @@ pragma solidity ^0.8.7;
 contract predMarket {
 
     address private immutable owner;
-    uint256 public BetA;
-    uint256 public BetB;
+    uint256 public BetA =0;
+    uint256 public BetB = 0;
     RaffleState private s_raffleState;
     uint256 public endTime;
     uint256 public startTime;
@@ -54,6 +54,12 @@ contract predMarket {
         CLOSED
     }
 
+    function viewPots()public view returns(uint256,uint256){
+        return (BetA,BetB);
+    }
+
+
+
     function emptyLosingArray(better[] storage arrLost, better[] storage arrWinner)private {
         //Responsible for paying the winner and dedudcting from the losers in the correct order
         uint i = 0;
@@ -81,6 +87,258 @@ contract predMarket {
         
     }
    }
+
+
+    function winLossBetA(uint256 potentialBet) public view returns(uint256,uint256, uint256) {
+        bool bettorExists = false;
+        uint256 potB = BetB; // Assuming BetB is declared somewhere in your contract
+        uint betterPosition;
+        better[] memory arrWinner;
+
+        // Check if bettor exists
+        for (uint i = 0; i < arrayOfBettersA.length; i++) {
+            if (msg.sender == arrayOfBettersA[i].bettor) {
+                bettorExists = true;
+                betterPosition = i;
+                break;
+            }
+        }
+
+        // Create arrWinner
+        if (!bettorExists) {
+            arrWinner = new better[](arrayOfBettersA.length + 1);
+            for (uint i = 0; i < arrayOfBettersA.length; i++) {
+                arrWinner[i] = arrayOfBettersA[i];
+            }
+            arrWinner[arrayOfBettersA.length] = better(payable(msg.sender), potentialBet, oddsBetA);
+            betterPosition = arrayOfBettersA.length;
+        } else {
+            arrWinner = arrayOfBettersA;
+            arrWinner[betterPosition].amount += potentialBet;
+            // Note: This does not actually modify the array; it's for calculation purposes
+        }
+
+        // Calculate winnings and risks
+        uint256 payout;
+        uint256 risk;
+        uint256 payout2;
+        for (uint i = 0; i < arrWinner.length; i++) {
+            payout = (arrWinner[i].amount * oddsBetA[0]) / oddsBetA[1];
+            payout2 = payout;
+            if (i == betterPosition) {
+                if (potB < payout) {
+                    return (payout2, potB, (potB * oddsBetA[1]) / oddsBetA[0]);
+                } else {
+                    risk = (payout * oddsBetA[1]) / oddsBetA[0];
+                    return (payout2, payout, arrWinner[i].amount);
+                }
+            } else {
+                if (potB >= payout) {
+                    potB -= payout;
+                }
+            }
+        }
+        return (payout2,0, 0); // Return default values if no condition is met
+    }
+
+    function winLossBetB(uint256 potentialBet) public view returns(uint256, uint256, uint256) {
+        bool bettorExists = false;
+        uint256 potA = BetA; // Assuming BetA is declared somewhere in your contract
+        uint betterPosition;
+        better[] memory arrWinner;
+
+        // Check if bettor exists
+        for (uint i = 0; i < arrayOfBettersB.length; i++) {
+            if (msg.sender == arrayOfBettersB[i].bettor) {
+                bettorExists = true;
+                betterPosition = i;
+                break;
+            }
+        }
+
+        // Create arrWinner
+        if (!bettorExists) {
+            arrWinner = new better[](arrayOfBettersB.length + 1);
+            for (uint i = 0; i < arrayOfBettersB.length; i++) {
+                arrWinner[i] = arrayOfBettersB[i];
+            }
+            arrWinner[arrayOfBettersB.length] = better(payable(msg.sender), potentialBet, oddsBetB);
+            betterPosition = arrayOfBettersB.length;
+        } else {
+            arrWinner = arrayOfBettersB;
+            arrWinner[betterPosition].amount += potentialBet;
+            // Note: This does not actually modify the array; it's for calculation purposes
+        }
+
+        // Calculate winnings and risks
+        uint256 payout;
+        uint256 risk;
+        uint256 payout2;
+        for (uint i = 0; i < arrWinner.length; i++) {
+            payout = (arrWinner[i].amount * oddsBetB[0]) / oddsBetB[1];
+            payout2 = payout;
+            if (i == betterPosition) {
+                if (potA < payout) {
+                    return (payout2, potA, (potA * oddsBetB[1]) / oddsBetB[0]);
+                } else {
+                    risk = (payout * oddsBetB[1]) / oddsBetB[0];
+                    return (payout2 ,payout, arrWinner[i].amount);
+                }
+            } else {
+                if (potA >= payout) {
+                    potA -= payout;
+                }
+            }
+        }
+        return (payout2, 0, 0); // Return default values if no condition is met
+    }
+
+
+//     function viewPotentialWinningsAndPotentialRiskA(uint256 potentialBet)public view returns(uint256,uint256) {
+//         //Responsible for paying the winner and dedudcting from the losers in the correct order
+//         uint i = 0;
+//         better[] memory arrWinner = arrayOfBettersA;
+//         better[] memory arrLost = arrayOfBettersB;
+
+//         uint j =0;
+//         uint arrOfWinner;
+//         uint previousBet = 0;
+
+        
+
+
+//         while(j<arrWinner.length){
+//             if(msg.sender == arrWinner[j].bettor){
+//                 arrOfWinner = j;
+//                 previousBet += arrWinner[j].amount + potentialBet;
+//                 arrWinner[j].amount += potentialBet;
+        
+//             }else{
+//             if(j+1 == arrWinner.length ){
+//                 better memory newbetter = better(payable(msg.sender),potentialBet,oddsBetA);
+
+//                 previousBet += potentialBet;
+//                 arrOfWinner = j+1;
+//                 arrWinner = new better[](j+2);
+//                 for(uint z = 0; z<=arrOfWinner;z++){
+//                     if(z == arrOfWinner){
+//                         arrWinner[z] = newbetter;
+//                     }else{
+//                         arrWinner[z] = arrayOfBettersA[z];
+//                     }
+                    
+//                 }
+//             }
+//             j++;
+//             }
+//         }
+
+//         while(i<arrWinner.length){
+           
+//             uint x = 0;
+//             uint winnerNeeds = (arrWinner[i].amount * arrWinner[i].odds[0])/arrWinner[i].odds[1] ;
+
+//             while (x < arrLost.length) {
+//                 if (arrLost[x].amount <= 0) {
+//                     x++; // Move to the next element if the current element is 0
+//                 } else if (arrLost[x].amount > winnerNeeds) {
+//                     arrLost[x].amount -= winnerNeeds;
+//                     arrWinner[i].amount += winnerNeeds;
+//                     break;
+//                      // Winner keeps original value and receives additional amount based on the odds
+//                     // Removed console.log as it's not supported in Solidity
+//                     // break; // Break is not necessary as return already exits the function
+//                 } else {
+//                     winnerNeeds -= arrLost[x].amount;
+//                     arrWinner[i].amount += arrLost[x].amount;
+//                     arrLost[x].amount = 0; // Set the current element to 0
+//                     x++; // Move to the next element
+//                 }
+//             }
+//             i++;
+        
+//     }
+//     uint256 reward = arrWinner[j].amount - previousBet;
+//     uint256 risk = (reward*arrWinner[0].odds[1])/arrWinner[0].odds[0];
+//     return(reward,risk);
+//     //Risk = (reward*odds[1])/odds[0]
+
+//    }
+
+//    function viewPotentialWinningsAndPotentialRiskB(uint256 potentialBet)public view returns(uint256,uint256) {
+//         //Responsible for paying the winner and dedudcting from the losers in the correct order
+//         uint i = 0;
+//         better[] memory arrWinner = arrayOfBettersB;
+//         better[] memory arrLost = arrayOfBettersA;
+
+//         uint j =0;
+//         uint arrOfWinner;
+//         uint previousBet = 0;
+
+        
+
+
+//         while(j<arrWinner.length){
+//             if(msg.sender == arrWinner[j].bettor){
+//                 arrOfWinner = j;
+//                 previousBet += arrWinner[j].amount + potentialBet;
+//                 arrWinner[j].amount += potentialBet;
+        
+//             }else{
+//             if(j+1 == arrWinner.length ){
+//                 better memory newbetter = better(payable(msg.sender),potentialBet,oddsBetB);
+
+//                 previousBet += potentialBet;
+//                 arrOfWinner = j+1;
+//                 arrWinner = new better[](j+2);
+//                 for(uint z = 0; z<=arrOfWinner;z++){
+//                     if(z == arrOfWinner){
+//                         arrWinner[z] = newbetter;
+//                     }else{
+//                         arrWinner[z] = arrayOfBettersB[z];
+//                     }
+                    
+//                 }
+//             }
+//             j++;
+//             }
+//         }
+
+//         while(i<arrWinner.length){
+           
+//             uint x = 0;
+//             uint winnerNeeds = (arrWinner[i].amount * arrWinner[i].odds[0])/arrWinner[i].odds[1] ;
+
+//             while (x < arrLost.length) {
+//                 if (arrLost[x].amount <= 0) {
+//                     x++; // Move to the next element if the current element is 0
+//                 } else if (arrLost[x].amount > winnerNeeds) {
+//                     arrLost[x].amount -= winnerNeeds;
+//                     arrWinner[i].amount += winnerNeeds;
+//                     break;
+//                      // Winner keeps original value and receives additional amount based on the odds
+//                     // Removed console.log as it's not supported in Solidity
+//                     // break; // Break is not necessary as return already exits the function
+//                 } else {
+//                     winnerNeeds -= arrLost[x].amount;
+//                     arrWinner[i].amount += arrLost[x].amount;
+//                     arrLost[x].amount = 0; // Set the current element to 0
+//                     x++; // Move to the next element
+//                 }
+//             }
+//             i++;
+        
+//     }
+//     uint256 reward = arrWinner[j].amount - previousBet;
+//     uint256 risk = (reward*arrWinner[0].odds[1])/arrWinner[0].odds[0];
+//     return(reward,risk);
+//     //Risk = (reward*odds[1])/odds[0]
+
+//    }
+
+   function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
 
 
     function assignEveryoneAmount() public onlyOwner{
@@ -167,6 +425,7 @@ contract predMarket {
         while(i<arrayOfBettersA.length){
             if(arrayOfBettersA[i].bettor == msg.sender){
                 arrayOfBettersA[i].amount+= msg.value;
+                BetA += msg.value;
                 return 0;
             }else{
                 i++;
@@ -185,6 +444,7 @@ contract predMarket {
         while(i<arrayOfBettersB.length){
             if(arrayOfBettersB[i].bettor == msg.sender){
                 arrayOfBettersB[i].amount+= msg.value;
+                BetB += msg.value;
                 return 0;
             }else{
                 i++;
@@ -203,9 +463,11 @@ contract predMarket {
         while(i<arrayOfBettersA.length){
             if(arrayOfBettersA[i].bettor==msg.sender){
                 if(arrayOfBettersA[i].amount>= reduction){
-                    arrayOfBettersA[i].amount -= reduction;
+                    
                     (bool success, ) = arrayOfBettersA[i].bettor.call{value: reduction}("");
                     require(success, "Transfer failed");
+                    arrayOfBettersA[i].amount -= reduction;
+                    BetA -= reduction;
                     break;
                 }else{
                     if(i == arrayOfBettersA.length -1 ){
@@ -225,9 +487,10 @@ contract predMarket {
         while(i<arrayOfBettersB.length){
             if(arrayOfBettersB[i].bettor==msg.sender){
                 if(arrayOfBettersB[i].amount>= reduction){
-                    arrayOfBettersB[i].amount -= reduction;
                     (bool success, ) = arrayOfBettersB[i].bettor.call{value: reduction}("");
                     require(success, "Transfer failed");
+                    arrayOfBettersB[i].amount -= reduction;
+                    BetB -= reduction;
                     break;
                 }else{
                     if(i == arrayOfBettersB.length -1 ){
@@ -288,8 +551,8 @@ contract predMarket {
 
     function endBet(uint8 wlc)public onlyOwner{
         //owner determines winner
-        require(s_raffleState== RaffleState.OPEN);
-        require(block.timestamp>=endTime);
+        // require(s_raffleState== RaffleState.OPEN);
+        // require(block.timestamp>=endTime);
 
         require(wlc==0||wlc==1||wlc==2,"invalid input");
 
@@ -346,6 +609,12 @@ contract predMarket {
             return false;
         }
     }
+
+    function getRaffleState() public view returns (RaffleState){
+        return s_raffleState;
+    }
+
+    
 
     
 
